4ac395c830efa9661145ea7fd38fea21f007d092
Esther2504 <esther252000@gmail.com>
2022-11-15 10:07:00 +0100

    linkedin & netflix project

--- portfolio/src/projectdata.js
+++ portfolio/src/projectdata.js
@@ -1,6 +1,14 @@
 export const data = [
   {
     "id": 1,
+    "img": "Netflix-Clone.png",
+    "name": "Netflix Clone",
+    "description": "Een clone van Netflix, gemaakt als groepsproject bij de web development opleiding. Bij dit project hebben wij gebruik gemaakt van React, Redux, Router en de TMBd API.",
+    "codelink": "https://github.com/tg-netflix",
+    "demolink": "https://tg-nepflix.vercel.app/"
+  },
+  {
+    "id": 2,
     "img": "WebShop.png",
     "name": "Webshop",
     "description": "Een webshop die hondenspullen verkoopt. Dit was het laatste soloproject van de web development opleiding. Bij dit project heb ik gebruik gemaakt van React, Redux, Router en Firebase.",
@@ -8,19 +16,20 @@ export const data = [
     "demolink": "https://esther2504.github.io/react-webshop/"
   },
   {
-    "id": 2,
+    "id": 3,
     "img": "Weather-App.png",
     "name": "Weather App",
     "description": "Een weerapp die het huidige weerbericht en de weersvoorspelling voor de komende vijf dagen weergeeft. Dit was mijn eerste React project. Bij dit project heb ik gebruik gemaakt van React en de Open Weather App API.",
     "codelink": "https://github.com/Esther2504/react-weather-app",
     "demolink": "https://esther2504.github.io/react-weather-app/"
-  },
-  {
-    "id": 3,
-    "img": "Memory.png",
-    "name": "Memory",
-    "description": "Een online memoryspel, waarvan het thema Fruit is. De score wordt bijgehouden en je kan een nieuwe game beginnen. Bij dit project heb ik gebruik gemaakt van HTML, CSS en JavaScript.",
-    "codelink": "https://codepen.io/esther222/pen/rNJQxOg",
-    "demolink": "https://fruitmemory.netlify.app/"
   }
+  // {
+  //   "id": 3,
+  //   "img": "Memory.png",
+  //   "name": "Memory",
+  //   "description": "Een online memoryspel, waarvan het thema Fruit is. De score wordt bijgehouden en je kan een nieuwe game beginnen. Bij dit project heb ik gebruik gemaakt van HTML, CSS en JavaScript.",
+  //   "codelink": "https://codepen.io/esther222/pen/rNJQxOg",
+  //   "demolink": "https://fruitmemory.netlify.app/"
+  // },
+
 ]
